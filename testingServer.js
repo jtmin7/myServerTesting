b8c893403ac40d087a6c01a2b50e5a462b12a6b7
cjcg <cjcg7@naver.com>
2019-10-11 09:53:36 +0900

    testingServer (node.js file) upload my first testingServer upload im start this file my server study

---
+++ testingServer.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const port = 80;
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello!');
+});
+server.listen(port, (err)=> {
+ 	if(err){
+		console.log(err);
+	}
+	let object_item = {1:1, b:'c'};
+	console.log(1, object_item);
+	console.log('Running Server');
+});
+
+setInterval(()=>{
+	console.log("Now Time is ", new Date());
+}, 2 * 1000);
