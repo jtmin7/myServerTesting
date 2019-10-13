66599afea1dbf26bb4d8786460a8d2fe3b7812c1
cjcg <cjcg7@naver.com>
2019-10-13 20:50:00 +0900

    test - NPM package.json test

---
+++ stdNote.js
@@ -0,0 +1,12 @@
+/*
+NPM automatically install package.json
+ make : 
+  npm init (-> automatically make package.json)
+ update :
+  npm install --Save color (-> automatically added json dependencies list)
+  npm uninstall --Save color (-> automatically deleted json dependencies list)
+
+
+*/
+  
+ 
