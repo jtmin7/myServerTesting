3425642caf9acb73a23aacc62b0e921692d96f8a
cjcg <cjcg7@naver.com>
2019-10-15 09:37:35 +0900

    std-longPoll
    server receives and updates the connection request every three seconds.
    server floats other sites according to the req.url given by the browser.
    
    Additional Implementation: Implementing a function that sends the html site to the browser

--- testingServer.js
+++ testingServer.js
@@ -2,20 +2,68 @@ const http = require('http');
 const port = 8000;
 const fs = require('fs');
 
+let count = 0;
 const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello!');
+	if(req.url == '/') {
+		// ** read html file
+		// ** I don't know why, but when connecting to the site with the script,
+		//   the HttpConnection increases the length.
+		let inputhtml = 'index.html';
+		showHtml(inputhtml, req, res);
+	// ** if url is like 'longPolling' 	
+	}else if(req.url == '/longpolling'){
+		// ** put req&res in HttpConnection
+		// ** In reality, you should use sesssion key values
+		//   to treat them with redis, cookies, ...
+		HttpConnection.push([req, res]);
+	// ** when unexpected url is rleased, the following site is posted
+	}else{
+		console.log(req.url);
+		let inputhtml = 'index02.html';
+		showHtml(inputhtml, req, res);	
+	}
 });
-server.listen(port, (err)=> {
- 	if(err){
+server.listen(80, (err)=>{
+	if(err){
 		console.log(err);
 	}
-	let object_item = {1:1, b:'c'};
-	console.log(1, object_item);
-	console.log('Running Server');
+	console.log('Server running');
+});
+// ** Send data over and over again every 30 Seconds
+let HttpConnection = [];
+setInterval(()=>{
+	// ** show size of data to be sent
+	console.log('httpConnection data length: [', HttpConnection.length, ']');
+	if(HttpConnection.length>0){
+		const Connection = HttpConnection.pop();
+		const res = Connection[1];
+		let str = 'Renewal by longPoll[' + HttpConnection.length + ']: '+ res;
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/plain');
+		// ** show res information to a client
+		res.end(str);
+	}
+}, 3*1000);
+
+const showHtml=((inputhtml, req, res)=>{
+	fs.readFile(inputhtml, 'utf8', (err, data) =>{
+		if(err){
+			res.statusCode = 404;
+			res.setHeader('Content-Type', 'text/plain');
+			res.end('File Not Found\n');
+		// ** if html file normal, send a html file
+		}else{ 
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'text/html');
+			res.end(data);
+		}
+	});
 });
 
+
+
+// ** book information 
+// ** p.107
 // **port 80: 127.0.0.1 
 // **port 8000: localhost:8000
 // **port 8800: localhost:8800
